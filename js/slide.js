520b94c35638e2ff77a7526593a1c354129b2bb8
Hariston Nunes Macedo <85318716+HaristonNuMacedo@users.noreply.github.com>
2021-12-26 11:32:22 -0300

    Slider Parte-3 "Sobre min".

---
+++ js/slide.js
@@ -0,0 +1,22 @@
+// Simple carousel
+// =============================================================================
+
+const $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+new Glider($simpleCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+   scrollLock: true,
+   scrollLockDelay: 100,
+   rewind: true,
+});
+
+// Responsive carousel
+// =============================================================================
+
